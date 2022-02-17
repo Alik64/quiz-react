6776be5b8ffca962ee5bd08e349c88449259c732
Albert <albert.barsamov@gmail.com>
2022-02-17 11:01:25 +0100

    Home page

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -3,24 +3,28 @@ import { createContext, useState, useEffect } from "react";
 export const AppContext = createContext()
 
 export function AppContextProvider(props) {
-    const [good, setGood] = useState(0)
+    const [score, setScore] = useState(0)
     const [err, setErr] = useState(0)
-
+    const [modal, setModal] = useState(true)
+    const [good, setGood] = useState(0)
 
     const chekerAnswer = bool => {
 
         if (bool) {
+            setScore(score + 1)
             setGood(good + 1)
         } else {
             if (err >= 5) {
-                setGood(good - 1)
+                setScore(score - 1)
             }
             setErr(err + 1)
-
         }
     }
+    const modalCloser = () => {
+        setModal(false)
+    }
     return (
-        <AppContext.Provider value={{ chekerAnswer, good, err }}>
+        <AppContext.Provider value={{ modalCloser, chekerAnswer, good, err, modal, score }}>
             {props.children}
         </AppContext.Provider>
     )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import logo from '../../assets/images/reactlogo.png'
 export default function Home() {
     return (
-        <div>Home</div>
+        <div className='home'>
+            <img src={logo} alt="react logo" className='home_logo' />
+            <h1 className='home_title'>Welcome to the ultimate React quiz!</h1>
+            <p>Let's discover how good your are !</p>
+            <Link to="/quiz"><button className='home_btn'>Let's go!</button></Link>
+        </div>
     )
 }
