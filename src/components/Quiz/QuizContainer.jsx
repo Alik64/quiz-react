c3fbe6ede6efe41dd685212086a3b4808a3c29b1
Albert <albert.barsamov@gmail.com>
2022-03-14 18:30:30 +0100

    refactoring quizReactContainer

--- src/components/Quiz/QuizContainer.jsx
+++ src/components/Quiz/QuizContainer.jsx
@@ -3,28 +3,11 @@ import Quiz from './Quiz.jsx'
 import { questions } from '../../questions'
 import reactlogo from '../../assets/images/reactlogo.png'
 
-const { rando, randoSequence } = require('@nastyox/rando.js');
+const { randoSequence } = require('@nastyox/rando.js');
 
-const useQuestions = (lngth) => {
-    // function getRandomNumber() {
-    //     return Math.floor(Math.random() * questions.length);
-    // }
-    // let newArr = []
-    // for (let i = 0; i < 15; i++) {
-    //     newArr.push(questions[getRandomNumber()])
-    // }
-    // return newArr
+const useRandomQuestions = (num) => {
 
-
-    // let arr = [];
-    // do {
-    //     let ran = Math.floor(Math.random() * questions.length);
-    //     arr = arr.indexOf(ran) > -1 ? arr : arr.concat(questions[ran]);
-    // } while (arr.length < lngth)
-
-    // return arr;
-
-    let randomNumArr = randoSequence(questions.length).slice(0, 15)
+    let randomNumArr = randoSequence(questions.length).slice(0, num)
 
     let newArr = []
 
@@ -42,7 +25,7 @@ const useQuestions = (lngth) => {
 
 export default function QuizContainer() {
 
-    const newArr = useQuestions(15)
+    const newArr = useRandomQuestions(15)
 
     return <Quiz
         title="React quiz"
