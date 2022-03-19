1147c950527169cd64e0281d3752a8ba48e2616b
Albert <albert.barsamov@gmail.com>
2022-03-19 14:19:12 +0100

    refactoring random number function

--- src/components/Quiz/QuizContainer.jsx
+++ src/components/Quiz/QuizContainer.jsx
@@ -2,19 +2,41 @@ import React from 'react'
 import Quiz from './Quiz.jsx'
 import { questions } from '../../questions'
 import reactlogo from '../../assets/images/reactlogo.png'
-const { randoSequence } = require('@nastyox/rando.js');
+
 
 const useQuestions = (arr, num, max) => {
 
-    const resultsArr = []
-    const randomNumbers = randoSequence(0, max).slice(0, num)
-    console.log(randomNumbers)
-    for (let i = 0; i < randomNumbers.length; i++) {
-        resultsArr.push(arr[randomNumbers[i]])
-    }
-    console.log(resultsArr)
+    // const resultsArr = []
+    // const randomNumbers = randoSequence(0, max).slice(0, num)
+    // console.log(randomNumbers)
+    // for (let i = 0; i < randomNumbers.length; i++) {
+    //     resultsArr.push(arr[randomNumbers[i]])
+    // }
+    // console.log(resultsArr)
 
-    return resultsArr;
+    // return resultsArr;
+
+    const resultsArr = []
+    const randomArr = []
+    const min = 0;
+    let verif, randomNumber
+
+    for (let i = 0; i < num; i++) {
+        do {
+            randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+            verif = randomArr.includes(randomNumber);
+            if (!verif) {
+                randomArr.push(randomNumber);
+            }
+        }
+        while (verif);
+    }
+    for (let j = 0; j < randomArr.length; j++) {
+        resultsArr.push(questions[randomArr[j]])
+    }
+
+
+    return resultsArr
 
 }
 
