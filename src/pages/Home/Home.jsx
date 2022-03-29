702d49c6781fcdecb7d508be901a7c5d7e86cf7a
Albert Barsamov <albert.barsamov@gmail.com>
2022-03-29 14:24:52 +0200

    Home page update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,6 +8,7 @@ export default function Home() {
             <h1 className='home_title'>Welcome to the ultimate React quiz!</h1>
             <p>Let's discover how good your are !</p>
             <Link to="/quiz"><button className='home_btn'>Let's go!</button></Link>
+            <p>Created by <a style={{color:'var(--darkBlue)'}}href="https://www.linkedin.com/in/albert-64/">Albert</a></p>
         </div>
     )
 }
