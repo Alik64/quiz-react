d179675ade7b71761a60d345b51cafb9c71bfa9b
Alik64 <albert.barsamov@gmail.com>
2022-05-07 22:30:00 +0200

    create custom Hook

---
+++ src/utils/customHook/useRandomItems.js
@@ -0,0 +1,28 @@
+// This function take an array and keep a given number of items selected randomly.
+
+// arr => Array to iterate.
+// num => How many items from `arr` to keep.
+// max => `arr` length.
+
+export const useRandomItems = (arr, num, max) => {
+  const resultsArr = [];
+  const randomArr = [];
+
+  const min = 0;
+
+  let verif, randomNumber;
+
+  for (let i = 0; i < num; i++) {
+    do {
+      randomNumber = Math.floor(Math.random() * (max - min)) + min;
+      verif = randomArr.includes(randomNumber);
+      if (!verif) {
+        randomArr.push(randomNumber);
+      }
+    } while (verif);
+  }
+  for (let j = 0; j < randomArr.length; j++) {
+    resultsArr.push(arr[randomArr[j]]);
+  }
+  return resultsArr;
+};
