3d6261eb811c47f2f69d9969389e934e3cf92be9
Alik64 <albert.barsamov@gmail.com>
2022-05-22 09:08:57 +0200

    ModalRules test

---
+++ src/components/Modals/ModalRules.test.js
@@ -0,0 +1,24 @@
+import ModalRules from "./ModalRules";
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { AppContextProvider } from "../../context/appContext";
+
+describe("<ModalRules/>", () => {
+  const modalRulesToogle = false;
+  beforeEach(() => {
+    render(
+      <AppContextProvider>
+        <ModalRules />
+      </AppContextProvider>
+    );
+  });
+
+  test("toggle button test", () => {
+    const toggleBtn = screen.getByTestId("toggle-btn");
+    expect(toggleBtn).toBeInTheDocument();
+    fireEvent.click(toggleBtn);
+    expect(screen.queryByTestId("toggle-elem")).toBeNull();
+  });
+
+  test("");
+});
