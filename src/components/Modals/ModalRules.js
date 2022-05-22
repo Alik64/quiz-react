f7b95da1052a2d4bf5b0ee85dc2fdae4ddacff11
Alik64 <albert.barsamov@gmail.com>
2022-05-22 09:09:09 +0200

    Modal rules Test

--- src/components/Modals/ModalRules.js
+++ src/components/Modals/ModalRules.js
@@ -14,22 +14,31 @@ export default function ModalRules() {
           </div>
           <div className="modal_text">
             <ul>
-              <li>You should answer to <strong>15</strong> questions.</li>
-              <li>Every positive answer give you <strong>1</strong> point.</li>
-              <li>Only <strong>1</strong> good answer by question.</li>
+              <li>
+                You should answer to <strong>15</strong> questions.
+              </li>
+              <li>
+                Every positive answer give you <strong>1</strong> point.
+              </li>
+              <li>
+                Only <strong>1</strong> good answer by question.
+              </li>
               <li>
                 If you make a mistake you can retry until you find a good
                 answer.
               </li>
-              <li>You have a right to make <strong>3</strong> errors.</li>
               <li>
-                After <strong>3rd</strong> mistake, each additional error will decrease your
-                score by <strong>1</strong> point.
+                You have a right to make <strong>3</strong> errors.
+              </li>
+              <li>
+                After <strong>3rd</strong> mistake, each additional error will
+                decrease your score by <strong>1</strong> point.
               </li>
             </ul>
 
             <h2>Good luck!</h2>
             <button
+              data-testid="toggle-btn"
               onClick={() => modalRulesToogle(false)}
               className="modal_btn home_btn"
             >
