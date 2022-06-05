a46ab8cee5a2d56d50e4ae1a1415640de83e8d09
Alik64 <albert.barsamov@gmail.com>
2022-06-05 21:56:01 +0200

    Font Family Roboto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Home from "./pages/Home/Home";
+
 import QuizContainer from "./components/Quiz/QuizContainer";
+import { HomeUI } from "./pages/Home/HomeUI";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<HomeUI />} />
       <Route path="quiz" element={<QuizContainer />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
