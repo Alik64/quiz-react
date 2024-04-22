ee5a3f701d12ffe107285c065b19dc094f7a1e9e
Albert Barsamov <albert.barsamov@gmail.com>
2024-04-22 11:57:33 +0200

    feat: update QuizContainer to fetch data

--- src/components/Quiz/QuizContainer.tsx
+++ src/components/Quiz/QuizContainer.tsx
@@ -1,27 +1,45 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Quiz from "./Quiz";
-
 import Preloader from "../Preloader/Preloader";
 
 export default function QuizContainer() {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");  
 
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
-      const result = await axios.get(
-        "https://quiz-react-jqocxvs1x-alik64s-projects.vercel.app/"
-      );
-      setItems(result.data);
+      setError("");  
+      const apiUrl = process.env.REACT_APP_API_URL;
+
+      if (!apiUrl) {
+        setError("API URL not defined");  
+        setLoading(false);
+        return;  
+      }
+
+      try {
+        const result = await axios.get(apiUrl);
+        setItems(result.data);
+      } catch (error) {
+        console.error('Failed to fetch data:', error);
+        setError("Failed to fetch data");  
+      }
       setLoading(false);
     };
+
     fetchData();
   }, []);
 
   if (loading) {
     return <Preloader />;
   }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return <Quiz title="React" newQuestions={items} />;
 }
