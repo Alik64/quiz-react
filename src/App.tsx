ef2688694ebe77c66225f6053fb0c442d19fcc78
Albert Barsamov <albert.barsamov@gmail.com>
2024-04-22 16:06:30 +0200

    feat: rollback for App

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import { lazy, Suspense } from "react";
+import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { HomeUI } from "./pages/Home/HomeUI";
 
-const QuizContainer = lazy(() => import("./components/Quiz/QuizContainer"));
-const Preloader = lazy(() => import("./components/Preloader/Preloader"));
+import QuizContainer from "./components/Quiz/QuizContainer";
+import { HomeUI } from "./pages/Home/HomeUI";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomeUI />} />
-        <Route
-          path="quiz"
-          element={
-            <Suspense fallback={<Preloader />}>
-              <QuizContainer />
-            </Suspense>
-          }
-        />
+        <Route path="quiz" element={<QuizContainer />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </>
